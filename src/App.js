4fdf53761b54dec839790ac2808aa739022be812
Brandondevster <92768663+Brandondevster@users.noreply.github.com>
2021-12-21 23:25:01 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import "./App.css";
+import NavBar from "./components/navBar";
+import Footer from "./components/footer";
+import Catalog from "./components/catalog";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <h1>Welcome to my fruit stand! </h1>
+        
+        <Catalog></Catalog>
+
+        <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
